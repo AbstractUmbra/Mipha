312f5fbd21c9b5e1da7cf84ab5c7ed694af2d07b
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2022-12-14 10:27:36 +0000

    Refactor time related commands into hybrids.
    Co-authored-by: avayert <ava@yert.pink>

--- extensions/time.py
+++ extensions/time.py
@@ -8,10 +8,12 @@ from __future__ import annotations
 
 import asyncio
 import datetime
+import difflib
 import zoneinfo
 from typing import TYPE_CHECKING, Literal, overload
 
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 from utilities import time
@@ -21,6 +23,8 @@ from utilities.fuzzy import extract
 
 if TYPE_CHECKING:
     from bot import Mipha
+
+AVAILABLE_TIMEZONES = {zone.replace("_", " "): zone for zone in zoneinfo.available_timezones()}
 
 
 class TimezoneConverter(commands.Converter[zoneinfo.ZoneInfo]):
@@ -53,6 +57,9 @@ class Time(commands.Cog):
 
     def __init__(self, bot: Mipha) -> None:
         self.bot: Mipha = bot
+        self._timezone_cache: list[app_commands.Choice[str]] = [
+            app_commands.Choice(name=name, value=val) for name, val in AVAILABLE_TIMEZONES.items()
+        ]
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild: discord.Guild) -> None:
@@ -103,61 +110,51 @@ class Time(commands.Cog):
             return dt_obj
         return time.hf_time(dt_obj)
 
-    @commands.command(aliases=["tz"])
-    async def timezone(
+    @commands.hybrid_group(invoke_without_command=True, fallback="get", aliases=["time", "tz"])
+    @app_commands.describe(member="The member to fetch the timezone of. Yours is shown if blank.")
+    async def timezone(self, ctx: Context, *, member: discord.Member | None = None) -> None:
+        """Get a member's stored timezone."""
+        if ctx.invoked_subcommand:
+            pass
+
+        if not ctx.guild:
+            await ctx.send("Sorry, this command doesn't work in DMs!", ephemeral=True)
+            return
+
+        full_member = member or ctx.author
+
+        async with ctx.typing(ephemeral=False):
+            query = """SELECT *
+                    FROM tz_store
+                    WHERE user_id = $1
+                    AND $2 = ANY(guild_ids);
+                    """
+            result = await self.bot.pool.fetchrow(query, full_member.id, ctx.guild.id)
+
+            if not result:
+                await ctx.send(f"No timezone for {full_member} set or it's not public in this guild.")
+                return
+
+            member_timezone = result["tz"]
+            tz = await TimezoneConverter().convert(ctx, member_timezone)
+            current_time = self._curr_tz_time(tz, ret_datetime=False)
+            embed = discord.Embed(title=f"Time for {full_member}", description=f"```\n{current_time}\n```")
+            embed.set_footer(text=member_timezone)
+            embed.timestamp = datetime.datetime.now(datetime.timezone.utc)
+
+        await ctx.send(embed=embed)
+
+    @timezone.command(name="set")
+    async def _set(
         self,
         ctx: Context,
         *,
         timezone: zoneinfo.ZoneInfo = commands.param(converter=TimezoneConverter),
     ) -> None:
-        """This will return the time in a specified timezone."""
-        embed = discord.Embed(
-            title=f"Current time in {timezone}",
-            description=f"```\n{self._curr_tz_time(timezone, ret_datetime=False)}\n```",
-        )
-        embed.set_footer(text=f"Requested by: {ctx.author}")
-        embed.timestamp = datetime.datetime.now(datetime.timezone.utc)
-        await ctx.send(embed=embed)
-
-    @commands.group(invoke_without_command=True)
-    @commands.guild_only()
-    async def time(self, ctx: Context, *, member: discord.Member | None = None) -> None:
-        """Let's look at storing member's tz."""
-        assert ctx.guild is not None
-
-        if ctx.invoked_subcommand:
-            pass
-
-        full_member = member or ctx.author
-        query = """SELECT *
-                   FROM tz_store
-                   WHERE user_id = $1
-                   AND $2 = ANY(guild_ids);
-                """
-        result = await self.bot.pool.fetchrow(query, full_member.id, ctx.guild.id)
-
-        if not result:
-            await ctx.send(f"No timezone for {full_member} set or it's not public in this guild.")
+        """Set your timezone publicly in this guild."""
+        if not ctx.guild:
+            await ctx.send("Sorry, this command only works in DMs!", ephemeral=True)
             return
-
-        member_timezone = result["tz"]
-        tz = await TimezoneConverter().convert(ctx, member_timezone)
-        current_time = self._curr_tz_time(tz, ret_datetime=False)
-        embed = discord.Embed(title=f"Time for {full_member}", description=f"```\n{current_time}\n```")
-        embed.set_footer(text=member_timezone)
-        embed.timestamp = datetime.datetime.now(datetime.timezone.utc)
-        await ctx.send(embed=embed)
-
-    @time.command(name="set")
-    @commands.guild_only()
-    async def _set(
-        self,
-        ctx: Context,
-        *,
-        set_timezone: zoneinfo.ZoneInfo = commands.param(converter=TimezoneConverter),
-    ) -> None:
-        """Add your time zone, with a warning about public info."""
-        assert ctx.guild is not None
 
         query = """ INSERT INTO tz_store(user_id, guild_ids, tz)
                     VALUES ($1, $2, $3)
@@ -165,21 +162,25 @@ class Time(commands.Cog):
                     SET guild_ids = tz_store.guild_ids || $2, tz = $3
                     WHERE tz_store.user_id = $1;
                 """
-        confirm = await ctx.prompt(
-            "This will make your timezone public in this guild, confirm?",
-        )
+        async with ctx.typing(ephemeral=True):
+            confirm = await ctx.prompt("This will make your timezone public in this guild, confirm?", delete_after=False)
 
-        if not confirm:
-            return
+            if not confirm:
+                return
 
-        await self.bot.pool.execute(query, ctx.author.id, [ctx.guild.id], set_timezone.key)
-        await ctx.message.add_reaction(ctx.tick(True))
+            await self.bot.pool.execute(query, ctx.author.id, [ctx.guild.id], timezone.key)
+            if ctx.interaction:
+                await ctx.interaction.edit_original_response(content="Done!")
+                return
 
-    @time.command(name="remove")
-    @commands.guild_only()
+            await ctx.send("Done!", ephemeral=True)
+
+    @timezone.command(name="remove")
     async def _remove(self, ctx: Context) -> None:
         """Remove your timezone from this guild."""
-        assert ctx.guild is not None
+        if not ctx.guild:
+            await ctx.send("Sorry, this command only works in DMs!", ephemeral=True)
+            return
 
         query = """
             WITH corrected AS (
@@ -195,18 +196,57 @@ class Time(commands.Cog):
             WHERE guild_ids <> new_guild_ids
             AND tz_store.user_id = corrected.user_id;
             """
-        await self.bot.pool.execute(query, ctx.author.id, ctx.guild.id)
-        await ctx.message.add_reaction(ctx.tick(True))
+        async with ctx.typing(ephemeral=True):
+            await self.bot.pool.execute(query, ctx.author.id, ctx.guild.id)
 
-    @time.command(name="clear")
+        await ctx.send("Done!", ephemeral=True)
+
+    @timezone.command(name="clear")
     async def _clear(self, ctx: Context) -> None:
         """Clears your timezones from all guilds."""
         query = "DELETE FROM tz_store WHERE user_id = $1;"
         confirm = await ctx.prompt("Are you sure you wish to purge your timezone from all guilds?")
         if not confirm:
             return
-        await self.bot.pool.execute(query, ctx.author.id)
-        await ctx.message.add_reaction(ctx.tick(True))
+
+        async with ctx.typing(ephemeral=True):
+            await self.bot.pool.execute(query, ctx.author.id)
+        await ctx.send("Done!", ephemeral=True)
+
+    @timezone.command(name="info", aliases=["tz"])
+    async def _info(
+        self,
+        ctx: Context,
+        *,
+        timezone: zoneinfo.ZoneInfo = commands.param(converter=TimezoneConverter),
+    ) -> None:
+        """This will return the time in a specified timezone."""
+        embed = discord.Embed(
+            title=f"Current time in {timezone}",
+            description=f"```\n{self._curr_tz_time(timezone, ret_datetime=False)}\n```",
+        )
+        embed.set_footer(text=f"Requested by: {ctx.author}")
+        embed.timestamp = datetime.datetime.now(datetime.timezone.utc)
+        await ctx.send(embed=embed)
+
+    @_info.autocomplete("timezone")
+    @_set.autocomplete("timezone")
+    async def timezone_autocomplete_callback(
+        self, interaction: discord.Interaction, current: str
+    ) -> list[app_commands.Choice[str]]:
+        if not current:
+            return self._timezone_cache[:25]
+
+        options: set[str] = set(AVAILABLE_TIMEZONES)
+
+        closest_matches = difflib.get_close_matches(word=current, possibilities=options, n=25, cutoff=0.6)
+
+        starts_with = [zone for zone in options.difference(closest_matches) if zone.lower().startswith(current.lower())]
+
+        cutoff = 25 - len(closest_matches)
+        view_order = starts_with[:cutoff] + closest_matches
+
+        return [app_commands.Choice(name=name, value=AVAILABLE_TIMEZONES[name]) for name in view_order]
 
     async def time_error(self, ctx: Context, error: commands.CommandError) -> None:
         """Quick error handling for timezones."""
