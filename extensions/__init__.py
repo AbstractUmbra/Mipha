0ece8085eb7da1d08fa90824c1430b3a8ba89d11
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2023-03-11 14:39:01 +0000

    fix module loading

--- extensions/__init__.py
+++ extensions/__init__.py
@@ -1,6 +1,25 @@
+import pathlib
 from pkgutil import ModuleInfo, iter_modules
 
 
-EXTENSIONS: list[ModuleInfo] = [
-    module for module in iter_modules(__path__, f"{__package__}.") if not module.name.startswith(f"{__package__}._")
-]
+_ext: list[ModuleInfo] = []
+_ext.extend(
+    [
+        module
+        for module in iter_modules(__path__, prefix=__package__ + ".")
+        if not module.name.rsplit(".")[-1].startswith("_")
+    ]
+)
+
+private_path = pathlib.Path(__package__ + "/private")
+if private_path.exists():
+    _ext.extend(
+        [
+            module
+            for module in iter_modules([str(private_path.absolute())], prefix=__package__ + ".private.")
+            if not module.name.rsplit(".")[-1].startswith("_")
+        ]
+    )
+
+
+EXTENSIONS = _ext
