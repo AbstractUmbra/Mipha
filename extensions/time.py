2c619f0c3500cfebfc39dac683346722d3f6cf35
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2023-04-08 05:44:08 +0100

    fix time stuff by following upstream

--- extensions/time.py
+++ extensions/time.py
@@ -6,28 +6,28 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from __future__ import annotations
 
-import asyncio
 import collections
 import datetime
-import difflib
 import zoneinfo
-from typing import TYPE_CHECKING, Literal, TypedDict, overload
+from typing import TYPE_CHECKING, Literal, NamedTuple, TypedDict, overload
 
 import discord
 from discord import app_commands
 from discord.ext import commands
+from lxml import etree
 
 from utilities import time
-from utilities.context import Context, Interaction
-from utilities.formats import random_pastel_colour
-from utilities.fuzzy import extract
+from utilities.cache import cache
+from utilities.context import Context, GuildContext, Interaction
+from utilities.formats import plural, random_pastel_colour
+from utilities.fuzzy import finder
 from utilities.paginator import RoboPages, SimpleListSource
 
 
 if TYPE_CHECKING:
-    from bot import Mipha
+    from typing_extensions import Self
 
-AVAILABLE_TIMEZONES = {zone.replace("_", " "): zone for zone in zoneinfo.available_timezones()}
+    from bot import Mipha
 
 
 class TimezoneRecord(TypedDict):
@@ -35,54 +35,72 @@ class TimezoneRecord(TypedDict):
     tz: str
 
 
-class TimezoneConverter(commands.Converter[zoneinfo.ZoneInfo]):
-    async def convert(self, ctx: Context, argument: str) -> zoneinfo.ZoneInfo:
-        query = extract(query=argument.lower(), choices=AVAILABLE_TIMEZONES.values(), limit=5)
-        if argument.lower() not in {timezone.lower() for timezone in AVAILABLE_TIMEZONES}:
-            matches = "\n".join([f"`{index}.` {match[0]}" for index, match in enumerate(query, start=1)])
-            question = await ctx.send(f"That was not a recognised timezone. Maybe you meant one of these?\n{matches}")
+class TimeZone(NamedTuple):
+    label: str
+    key: str
 
-            def check(message: discord.Message) -> bool:
-                return (
-                    message.author == ctx.author
-                    and message.channel == ctx.channel
-                    and message.content.removesuffix(".").isdigit()
-                    and 1 >= int(message.content.removesuffix(".")) <= 5
-                )
+    @classmethod
+    async def convert(cls, ctx: Context, argument: str) -> Self:
+        assert isinstance(ctx.cog, Time)
 
-            try:
-                result: discord.Message = await ctx.bot.wait_for("message", check=check, timeout=30)
-            except asyncio.TimeoutError:
-                await question.delete()
-                raise commands.BadArgument("No valid timezone given or selected.")
+        # Prioritise aliases because they handle short codes slightly better
+        if argument in ctx.cog._timezone_aliases:
+            return cls(key=argument, label=ctx.cog._timezone_aliases[argument])
 
-            return zoneinfo.ZoneInfo(query[int(result.content) - 1][0])
+        if argument in ctx.cog.valid_timezones:
+            return cls(key=argument, label=argument)
 
-        return zoneinfo.ZoneInfo(AVAILABLE_TIMEZONES[argument])
+        timezones = ctx.cog.find_timezones(argument)
+
+        try:
+            return await ctx.disambiguate(timezones, lambda t: t[0], ephemeral=True)
+        except ValueError:
+            raise commands.BadArgument(f"Could not find timezone for {argument!r}")
+
+    def to_choice(self) -> app_commands.Choice[str]:
+        return app_commands.Choice(name=self.label, value=self.key)
+
+    def to_zone(self) -> zoneinfo.ZoneInfo:
+        return zoneinfo.ZoneInfo(self.key)
 
 
-class TimezoneSource(SimpleListSource):
-    def format_page(self, _: RoboPages, entries: list[tuple[str, str, datetime.timedelta]]) -> discord.Embed:
+class CLDRDataEntry(NamedTuple):
+    description: str
+    aliases: list[str]
+    deprecated: bool
+    preferred: str | None
+
+
+class TimezoneSource(SimpleListSource[tuple[str, datetime.timedelta]]):
+    def format_page(self, _: RoboPages, entries: list[tuple[str, datetime.timedelta]]) -> discord.Embed:
         embed = discord.Embed(title="Dannyware Timezones!", colour=random_pastel_colour())
-        tz_dict = collections.defaultdict(list)
+        tz_dict: collections.defaultdict[int, list[str]] = collections.defaultdict(list)
 
         def to_hour(td: datetime.timedelta) -> int:
             seconds = round(td.total_seconds())
             return seconds // (60 * 60)
 
-        for member_str, dt_string, offset in entries:
+        for member_str, offset in entries:
             if not offset:
                 offset = datetime.timedelta(0)
 
             houred_offset = to_hour(offset)
-            tz_dict[houred_offset].append(f"{member_str}: {dt_string}")
+            tz_dict[houred_offset].append(member_str)
 
+        now = discord.utils.utcnow()
         for key, value in sorted(tz_dict.items()):
             fmt = ""
             for idx, member in enumerate(value, start=1):
-                fmt += f"{idx}. {member}"
-            name = "UTC" if key == 0 else f"{key} hours from UTC"
-            embed.add_field(name=name, value=fmt, inline=False)
+                fmt += f"{idx}. {member}\n"
+            name = "UTC" if key == 0 else f"{plural(abs(key)):hour} {'ahead of' if key > 0 else 'behind'} UTC"
+            dt = now + datetime.timedelta(hours=key)
+            ordinal_ = time.ordinal(dt.day)
+            time_fmt = dt.strftime(f"%A {ordinal_} of %B %Y at %H:%M")
+            embed.add_field(
+                name=f"{name} ({time_fmt})",
+                value=fmt,
+                inline=False,
+            )
 
         embed.timestamp = discord.utils.utcnow()
 
@@ -90,19 +108,138 @@ class TimezoneSource(SimpleListSource):
 
 
 class Time(commands.Cog):
-    """Time cog for fun time stuff."""
+    """Cog for timezone related things."""
+
+    DEFAULT_POPULAR_TIMEZONE_IDS = (
+        # America
+        "usnyc",  # America/New_York
+        "uslax",  # America/Los_Angeles
+        "uschi",  # America/Chicago
+        "usden",  # America/Denver
+        # India
+        "inccu",  # Asia/Kolkata
+        # Europe
+        "trist",  # Europe/Istanbul
+        "rumow",  # Europe/Moscow
+        "gblon",  # Europe/London
+        "frpar",  # Europe/Paris
+        "esmad",  # Europe/Madrid
+        "deber",  # Europe/Berlin
+        "grath",  # Europe/Athens
+        "uaiev",  # Europe/Kyev
+        "itrom",  # Europe/Rome
+        "nlams",  # Europe/Amsterdam
+        "plwaw",  # Europe/Warsaw
+        # Canada
+        "cator",  # America/Toronto
+        # Australia
+        "aubne",  # Australia/Brisbane
+        "ausyd",  # Australia/Sydney
+        # Brazil
+        "brsao",  # America/Sao_Paulo
+        # Japan
+        "jptyo",  # Asia/Tokyo
+        # China
+        "cnsha",  # Asia/Shanghai
+    )
 
     def __init__(self, bot: Mipha) -> None:
         self.bot: Mipha = bot
-        self._timezone_cache: list[app_commands.Choice[str]] = [
-            app_commands.Choice(name=name, value=val) for name, val in AVAILABLE_TIMEZONES.items()
-        ]
+        self.valid_timezones: set[str] = zoneinfo.available_timezones()
+        self._timezone_aliases: dict[str, str] = {
+            "Eastern Time": "America/New_York",
+            "Central Time": "America/Chicago",
+            "Mountain Time": "America/Denver",
+            "Pacific Time": "America/Los_Angeles",
+            # (Unfortunately) special case American timezone abbreviations
+            "EST": "America/New_York",
+            "CST": "America/Chicago",
+            "MST": "America/Denver",
+            "PST": "America/Los_Angeles",
+            "EDT": "America/New_York",
+            "CDT": "America/Chicago",
+            "MDT": "America/Denver",
+            "PDT": "America/Los_Angeles",
+        }
+        self._default_timezones: list[app_commands.Choice[str]] = []
+
+    async def cog_load(self) -> None:
+        await self.parse_bcp47_timezones()
 
     async def cog_command_error(self, ctx: Context, error: commands.CommandError) -> None:
         """Error handling for Time.py."""
         error = getattr(error, "original", error)
         if isinstance(error, commands.BadArgument):
             await ctx.send(str(error))
+
+    async def parse_bcp47_timezones(self) -> None:
+        async with self.bot.session.get(
+            "https://raw.githubusercontent.com/unicode-org/cldr/main/common/bcp47/timezone.xml"
+        ) as resp:
+            if resp.status != 200:
+                return
+
+            parser = etree.XMLParser(ns_clean=True, recover=True, encoding="utf-8")
+            tree = etree.fromstring(await resp.read(), parser=parser)
+
+            # Build a temporary dictionary to resolve "preferred" mappings
+            entries: dict[str, CLDRDataEntry] = {
+                node.attrib["name"]: CLDRDataEntry(
+                    description=node.attrib["description"],
+                    aliases=node.get("alias", "Etc/Unknown").split(" "),
+                    deprecated=node.get("deprecated", "false") == "true",
+                    preferred=node.get("preferred"),
+                )
+                for node in tree.iter("type")
+                # Filter the Etc/ entries (except UTC)
+                if not node.attrib["name"].startswith(("utcw", "utce", "unk"))
+                and not node.attrib["description"].startswith("POSIX")
+            }
+
+            for entry in entries.values():
+                # These use the first entry in the alias list as the "canonical" name to use when mapping the
+                # timezone to the IANA database.
+                # The CLDR database is not particularly correct when it comes to these, but neither is the IANA database.
+                # It turns out the notion of a "canonical" name is a bit of a mess. This works fine for users where
+                # this is only used for display purposes, but it's not ideal.
+                if entry.preferred is not None:
+                    preferred = entries.get(entry.preferred)
+                    if preferred is not None:
+                        self._timezone_aliases[entry.description] = preferred.aliases[0]
+                else:
+                    self._timezone_aliases[entry.description] = entry.aliases[0]
+
+            for key in self.DEFAULT_POPULAR_TIMEZONE_IDS:
+                entry = entries.get(key)
+                if entry is not None:
+                    self._default_timezones.append(app_commands.Choice(name=entry.description, value=entry.aliases[0]))
+
+    @cache()
+    async def get_timezone(self, user_id: int, /) -> str | None:
+        query = "SELECT tz from tz_store WHERE user_id = $1;"
+        record = await self.bot.pool.fetchrow(query, user_id)
+        return record["tz"] if record else None
+
+    async def get_tzinfo(self, user_id: int, /) -> datetime.tzinfo:
+        tz = await self.get_timezone(user_id)
+        if tz is None:
+            return datetime.timezone.utc
+
+        try:
+            tz = zoneinfo.ZoneInfo(tz)
+        except zoneinfo.ZoneInfoNotFoundError:
+            tz = datetime.timezone.utc
+
+        return tz
+
+    def find_timezones(self, query: str) -> list[TimeZone]:
+        # A bit hacky, but if '/' is in the query then it's looking for a raw identifier
+        # otherwise it's looking for a CLDR alias
+        if "/" in query:
+            return [TimeZone(key=a, label=a) for a in finder(query, self.valid_timezones)]
+
+        keys = finder(query, self._timezone_aliases.keys())
+        return [TimeZone(label=k, key=self._timezone_aliases[k]) for k in keys]
 
     def _gen_tz_embeds(self, requester: str, iterable: list[str]) -> list[discord.Embed]:
         embeds = []
@@ -151,8 +288,8 @@ class Time(commands.Cog):
                 return
 
             member_timezone = result["tz"]
-            tz = await TimezoneConverter().convert(ctx, member_timezone.replace("_", " "))
-            current_time = self._curr_tz_time(tz, ret_datetime=False)
+            tz = await TimeZone.convert(ctx, member_timezone)
+            current_time = self._curr_tz_time(zoneinfo.ZoneInfo(tz.key), ret_datetime=False)
             embed = discord.Embed(title=f"Time for {full_member}", description=f"```\n{current_time}\n```")
             embed.set_footer(text=member_timezone)
             embed.timestamp = datetime.datetime.now(datetime.timezone.utc)
@@ -164,18 +301,14 @@ class Time(commands.Cog):
         self,
         ctx: Context,
         *,
-        timezone: zoneinfo.ZoneInfo = commands.param(converter=TimezoneConverter),
+        timezone: TimeZone,
     ) -> None:
-        """Set your timezone publicly in this guild. Please use formats like:-
+        """Set your timezone publicly. Please use formats like:-
 
         `America/New York`
         `Europe/London`
         `Asia/Tokyo`
         """
-        if not ctx.guild:
-            await ctx.send("Sorry, this command only works in DMs!", ephemeral=True)
-            return
-
         query = """ INSERT INTO tz_store(user_id, tz)
                     VALUES ($1, $2)
                     ON CONFLICT (user_id) DO UPDATE
@@ -183,7 +316,9 @@ class Time(commands.Cog):
                     WHERE tz_store.user_id = $1;
                 """
         async with ctx.typing(ephemeral=True):
-            confirm = await ctx.prompt("This will make your timezone public in this guild, confirm?", delete_after=False)
+            confirm = await ctx.prompt(
+                f"This will make {timezone.label!r} your public timezone public, confirm?", delete_after=False
+            )
 
             if not confirm:
                 return
@@ -197,11 +332,7 @@ class Time(commands.Cog):
 
     @timezone.command(name="remove")
     async def _remove(self, ctx: Context) -> None:
-        """Remove your timezone from this guild."""
-        if not ctx.guild:
-            await ctx.send("Sorry, this command only works in DMs!", ephemeral=True)
-            return
-
+        """Remove your timezone from the bot."""
         query = """
             DELETE *
             FROM tz_store
@@ -213,13 +344,11 @@ class Time(commands.Cog):
         await ctx.send("Done!", ephemeral=True)
 
     @timezone.command(name="info", aliases=["tz"])
-    async def _info(
-        self, ctx: Context, *, timezone: zoneinfo.ZoneInfo = commands.param(converter=TimezoneConverter)
-    ) -> None:
+    async def _info(self, ctx: Context, *, timezone: TimeZone) -> None:
         """This will return the time in a specified timezone."""
         embed = discord.Embed(
             title=f"Current time in {timezone}",
-            description=f"```\n{self._curr_tz_time(timezone, ret_datetime=False)}\n```",
+            description=f"```\n{self._curr_tz_time(timezone.to_zone(), ret_datetime=False)}\n```",
         )
         embed.set_footer(text=f"Requested by: {ctx.author}")
         embed.timestamp = datetime.datetime.now(datetime.timezone.utc)
@@ -229,18 +358,10 @@ class Time(commands.Cog):
     @_set.autocomplete("timezone")
     async def timezone_autocomplete_callback(self, interaction: Interaction, current: str) -> list[app_commands.Choice[str]]:
         if not current:
-            return self._timezone_cache[:25]
+            return self._default_timezones
 
-        options: set[str] = set(AVAILABLE_TIMEZONES)
-
-        closest_matches = difflib.get_close_matches(word=current, possibilities=options, n=25, cutoff=0.6)
-
-        starts_with = [zone for zone in options.difference(closest_matches) if zone.lower().startswith(current.lower())]
-
-        cutoff = 25 - len(closest_matches)
-        view_order = starts_with[:cutoff] + closest_matches
-
-        return [app_commands.Choice(name=name, value=AVAILABLE_TIMEZONES[name]) for name in view_order]
+        matches = self.find_timezones(current)
+        return [tz.to_choice() for tz in matches[:25]]
 
     async def time_error(self, ctx: Context, error: commands.CommandError) -> None:
         """Quick error handling for timezones."""
@@ -248,19 +369,10 @@ class Time(commands.Cog):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("How am I supposed to do this if you don't supply the timezone?")
 
-    async def _fetch_time_records(self, guild: discord.Guild, /) -> list[TimezoneRecord]:
-        query = """
-        SELECT user_id, tz
-        FROM tz_store
-        WHERE $1 = ANY(guild_ids);
-        """
-
-        return await self.bot.pool.fetch(query, guild.id)
-
     def _transform_records(
         self, records: list[TimezoneRecord], *, guild: discord.Guild
-    ) -> list[tuple[str, str, datetime.timedelta]]:
-        ret: list[tuple[str, str, datetime.timedelta]] = []
+    ) -> list[tuple[str, datetime.timedelta]]:
+        ret: list[tuple[str, datetime.timedelta]] = []
         for record in records:
             user_id = record["user_id"]
             user_ = guild.get_member(user_id)
@@ -272,13 +384,10 @@ class Time(commands.Cog):
             tz = record["tz"]
             timezone = zoneinfo.ZoneInfo(tz)
             offset = timezone.utcoffset(datetime.datetime.now(datetime.timezone.utc)) or datetime.timedelta(0)
-            dt = datetime.datetime.now(timezone)
-            ordinal_ = time.ordinal(dt.day)
-            fmt = dt.strftime(f"%A {ordinal_} of %B %Y at %H:%M")
 
-            ret.append((user, fmt, offset))
+            ret.append((user, offset))
 
-        ret.sort(key=lambda t: t[2])
+        ret.sort(key=lambda t: t[1])
 
         return ret
 
@@ -292,8 +401,13 @@ class Time(commands.Cog):
 
         await interaction.response.defer(ephemeral=True)
 
-        context = await Context.from_interaction(interaction)
-        records: list[TimezoneRecord] = await self._fetch_time_records(interaction.guild)
+        context = await GuildContext.from_interaction(interaction)
+        query = """
+                SELECT user_id, tz
+                FROM tz_store
+                """
+        records: list[TimezoneRecord] = await self.bot.pool.fetch(query)
+        records = [*filter(lambda r: r["user_id"] in [m.id for m in context.guild.members], records)]
         if not records:
             return await interaction.followup.send("Sorry but there are no recorded timezones here!", ephemeral=True)
         transformed = self._transform_records(records, guild=interaction.guild)
