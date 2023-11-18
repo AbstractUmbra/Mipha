1f0be05d5acfe1b6653cdbb3debd4623a0fb35ed
Alex Nørgaard <umbra@abstractumbra.dev>
2023-11-18 00:07:32 +0000

    resolve submodule finding

--- extensions/__init__.py
+++ extensions/__init__.py
@@ -1,14 +1,10 @@
 import pathlib
 from pkgutil import ModuleInfo, iter_modules
 
-_ext: list[ModuleInfo] = []
-_ext.extend(
-    [
-        module
-        for module in iter_modules(__path__, prefix=__package__ + ".")
-        if not module.name.rsplit(".")[-1].startswith("_")
-    ]
-)
+_ext: list[ModuleInfo] = [
+    module for module in iter_modules(__path__, prefix=__package__ + ".") if not module.name.rsplit(".")[-1].startswith("_")
+]
+
 
 private_path = pathlib.Path(__package__ + "/private")
 if private_path.exists():
