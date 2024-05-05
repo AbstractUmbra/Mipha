8b8bee8ec1fb1e47cb81bfd806aea7fc4cec97c1
Alex Nørgaard <umbra@abstractumbra.dev>
2024-05-05 14:33:23 +0100

    move primary config to non-shared dir

---
+++ utilities/_types/config.py
@@ -0,0 +1,118 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, TypedDict
+
+if TYPE_CHECKING:
+    from typing import NotRequired, Required
+
+__all__ = ("RootConfig",)
+
+
+class _RTFMPackage(TypedDict):
+    package: str
+    base_url: str
+    inventory_url: str
+
+
+class BotConfig(TypedDict):
+    token: str
+    dev_guilds: NotRequired[list[int]]
+
+
+class RTFMConfig(TypedDict):
+    packages: list[_RTFMPackage]
+
+
+class DatabaseConfig(TypedDict, total=False):
+    dsn: Required[str]
+    audio_dsn: str
+    host: str
+    user: str
+    password: str
+    database: str
+    port: int
+
+
+class RedisConfig(TypedDict):
+    url: str
+    port: int
+    password: str
+    mock: bool
+
+
+class WebhookConfig(TypedDict):
+    logging: str
+    mangadex: NotRequired[str]
+
+
+class TokenConfig(TypedDict, total=False):
+    mystbin: str
+    github: str
+    wanikani: str
+    sonarr: str
+    tiktok: str
+    pythonista_api: str
+
+
+class MangaDexConfig(TypedDict):
+    username: str
+    password: str
+    client_id: str
+    client_secret: str
+
+
+class UploaderConfig(TypedDict):
+    token: str
+
+
+class CurrencyConfig(TypedDict):
+    api_key: str
+
+
+class DeeplConfig(TypedDict):
+    api_key: str
+
+
+class RCONConfig(TypedDict):
+    host: str
+    password: str
+    port: int
+
+
+class DucklingConfig(TypedDict):
+    host: str
+    port: int
+
+
+class _BooruConfig(TypedDict):
+    api_key: str
+    user_id: str
+
+
+class LewdConfig(TypedDict):
+    gelbooru: _BooruConfig
+    danbooru: _BooruConfig
+
+
+class Logging(TypedDict):
+    dm: bool
+    webhooks: list[str]
+
+
+class RootConfig(TypedDict, total=False):
+    bot: Required[BotConfig]
+    rtfm: Required[RTFMConfig]
+    owner_ids: Required[list[int]]
+    intents: int
+    postgresql: Required[DatabaseConfig]
+    redis: RedisConfig
+    webhooks: Required[WebhookConfig]
+    tokens: TokenConfig
+    mangadex: MangaDexConfig
+    uploader: UploaderConfig
+    deepl: DeeplConfig
+    currency: CurrencyConfig
+    rcon: RCONConfig
+    duckling: DucklingConfig
+    lewd: LewdConfig
+    logging_webhooks: dict[str, Logging]  # guild_id: [channels]
