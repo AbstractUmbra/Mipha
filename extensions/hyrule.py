f3bfea7dc767ee2680ee0835881d823309ad72c6
Alex Nørgaard <umbra@abstractumbra.dev>
2024-05-14 13:39:41 +0100

    cleanup hyrule management

--- extensions/hyrule.py
+++ extensions/hyrule.py
@@ -1,20 +1,28 @@
 from __future__ import annotations
 
+import logging
+import re
 from typing import TYPE_CHECKING
 
 import discord
 from discord.ext import commands
 
+from utilities.shared.checks import restricted_guild
+from utilities.shared.formats import random_pastel_colour
 from utilities.shared.ui import BaseView
 
 HYRULE_GUILD_ID = 705500489248145459
 _IRL_FRIEND_SERVER = 174702278673039360
 HONDANA_ROLE_ID = 1086537644093231144
 GREAT_ASSET_ROLE_ID = 1189005762790441010
-HELLDIVERS_2_ROLE_ID = 1217496141311250572
+BOT_BAIT_CHANNEL_ID = 1238074949424386121
+RULES_CHANNEL_ID = 1238076485600935977
+RULES_MESSAGE_ID = 1238077226528800779
 ROLE_ASSIGNMENT_CHANNEL_ID = 1086540538112647229
 ROLE_ASSIGNMENT_MESSAGE_ID = 1086545767356977173
 
+RULE_REGEX: re.Pattern[str] = re.compile(r"(?P<num>\d)\.\s(?P<content>.*)(?:\r|\n)?")
+LOGGER = logging.getLogger(__name__)
 
 if TYPE_CHECKING:
     from typing import Self
@@ -84,33 +92,36 @@ class HyruleRoleAssignmentView(BaseView):
 
         await interaction.user.add_roles(discord.Object(id=GREAT_ASSET_ROLE_ID))
 
-    @discord.ui.button(
-        label="Helldivers 2",
-        custom_id="HyruleHelldivers__",
-        style=discord.ButtonStyle.red,
-        emoji="\U0001f41b",
-        row=1,
-    )
-    async def add_helldivers_role(self, interaction: Interaction, item: discord.ui.Button[Self]) -> None:
-        """This role will allow {owner} to ping you to go helldiving with them!"""
-        assert isinstance(interaction.user, discord.Member)
-
-        if interaction.user.get_role(HELLDIVERS_2_ROLE_ID):
-            await interaction.response.send_message("You already have the Helldivers 2 role!", ephemeral=True)
-            return
-
-        await interaction.user.add_roles(discord.Object(id=HELLDIVERS_2_ROLE_ID))
-
 
 class Hyrule(commands.Cog):
     def __init__(self, bot: Mipha, /) -> None:
         self.bot: Mipha = bot
         self.bot.loop.create_task(self._assign_view())
+        self.rules: dict[int, str] = {}
 
     async def _assign_view(self) -> None:
         await self.bot.wait_until_ready()
         self.view: HyruleRoleAssignmentView = HyruleRoleAssignmentView(self.bot)
         self.bot.add_view(self.view, message_id=ROLE_ASSIGNMENT_MESSAGE_ID)
+
+    def _parse_rules(self, content: str, /) -> None:
+        matches = list(RULE_REGEX.finditer(content))
+
+        self.rules = {int(m["num"]): m["content"] for m in matches}
+
+    async def _load_rules(self) -> None:
+        if self.rules:
+            return
+
+        channel = self.bot.get_partial_messageable(RULES_CHANNEL_ID, guild_id=HYRULE_GUILD_ID, type=discord.ChannelType.text)
+        partial_message = channel.get_partial_message(RULES_MESSAGE_ID)
+
+        message = await partial_message.fetch()
+        rules_content = message.content
+
+        LOGGER.info("Parsed rules: %s", "\n".join(rules_content.split("\n")))
+
+        self._parse_rules(rules_content)
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member) -> None:
@@ -120,8 +131,18 @@ class Hyrule(commands.Cog):
         if member.bot:
             await member.add_roles(discord.Object(id=929886067178504234))
 
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message) -> None:
+        if not message.guild or message.guild.id != HYRULE_GUILD_ID:
+            return
+
+        assert isinstance(message.author, discord.Member)
+
+        if message.channel.id == BOT_BAIT_CHANNEL_ID:
+            await message.author.ban(delete_message_days=1, reason="Compromised account, fell for the bait.")
+
     @commands.is_owner()
-    @commands.guild_only()
+    @restricted_guild(HYRULE_GUILD_ID)
     @commands.command()
     async def setup_role_assignments(self, ctx: GuildContext) -> None:
         """This command will quickly update the role assignment message with the in-memory View created in this same extension."""
@@ -137,6 +158,23 @@ class Hyrule(commands.Cog):
             content=content,
             view=self.view,
         )
+
+    @commands.guild_only()
+    @restricted_guild(HYRULE_GUILD_ID)
+    @commands.group(name="rules", aliases=["rule"])
+    async def rules_command(self, ctx: GuildContext, rule_num: int) -> None:
+        if not self.rules:
+            await self._load_rules()
+
+        try:
+            rule = self.rules[rule_num]
+        except KeyError:
+            return await ctx.send(f"Sorry, but we don't have a rule #{rule_num}", delete_after=5)
+
+        embed = discord.Embed(
+            title=f"Rule {rule_num}", colour=random_pastel_colour(), description=rule, timestamp=ctx.message.created_at
+        )
+        await ctx.send(embed=embed)
 
 
 async def setup(bot: Mipha) -> None:
