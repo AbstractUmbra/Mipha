ab881dbd232d8472272a8ea540d9d8fe173fc699
Alex Nørgaard <umbra@abstractumbra.dev>
2025-03-21 05:06:43 +0000

    correct timezone handler usage

--- extensions/time.py
+++ extensions/time.py
@@ -10,19 +10,18 @@ import collections
 import datetime
 import operator
 import zoneinfo
-from typing import TYPE_CHECKING, Literal, NamedTuple, TypedDict, overload
+from typing import TYPE_CHECKING, Literal, TypedDict, overload
 
 import discord
 from discord import app_commands
 from discord.ext import commands
-from lxml import etree  # pyright: ignore[reportAttributeAccessIssue] # lxml doesn't re-export anything so this breaks in CI
 
 from utilities.context import Context, GuildContext, Interaction
 from utilities.shared import time
 from utilities.shared.cache import cache
 from utilities.shared.formats import plural, random_pastel_colour
-from utilities.shared.fuzzy import finder
 from utilities.shared.paginator import RoboPages, SimpleListSource
+from utilities.shared.timezones import TimeZone
 
 if TYPE_CHECKING:
     from bot import Mipha
@@ -31,43 +30,6 @@ if TYPE_CHECKING:
 class TimezoneRecord(TypedDict):
     user_id: int
     tz: str
-
-
-class TimeZone(NamedTuple):
-    label: str
-    key: str
-
-    @classmethod
-    async def convert(cls, ctx: Context, argument: str) -> TimeZone:
-        assert isinstance(ctx.cog, Time)
-
-        # Prioritise aliases because they handle short codes slightly better
-        if argument in ctx.cog._timezone_aliases:
-            return cls(key=argument, label=ctx.cog._timezone_aliases[argument])
-
-        if argument in ctx.cog.valid_timezones:
-            return cls(key=argument, label=argument)
-
-        timezones = ctx.cog.find_timezones(argument)
-
-        try:
-            return await ctx.disambiguate(timezones, operator.itemgetter(0), ephemeral=True)
-        except ValueError as err:
-            msg = f"Could not find timezone for {argument!r}"
-            raise commands.BadArgument(msg) from err
-
-    def to_choice(self) -> app_commands.Choice[str]:
-        return app_commands.Choice(name=self.label, value=self.key)
-
-    def to_zone(self) -> zoneinfo.ZoneInfo:
-        return zoneinfo.ZoneInfo(self.key)
-
-
-class CLDRDataEntry(NamedTuple):
-    description: str
-    aliases: list[str]
-    deprecated: bool
-    preferred: str | None
 
 
 class TimezoneSource(SimpleListSource[tuple[str, datetime.timedelta]]):
@@ -108,109 +70,14 @@ class TimezoneSource(SimpleListSource[tuple[str, datetime.timedelta]]):
 class Time(commands.Cog):
     """Cog for timezone related things."""
 
-    DEFAULT_POPULAR_TIMEZONE_IDS = (
-        # America
-        "usnyc",  # America/New_York
-        "uslax",  # America/Los_Angeles
-        "uschi",  # America/Chicago
-        "usden",  # America/Denver
-        # India
-        "inccu",  # Asia/Kolkata
-        # Europe
-        "trist",  # Europe/Istanbul
-        "rumow",  # Europe/Moscow
-        "gblon",  # Europe/London
-        "frpar",  # Europe/Paris
-        "esmad",  # Europe/Madrid
-        "deber",  # Europe/Berlin
-        "grath",  # Europe/Athens
-        "uaiev",  # Europe/Kyev
-        "itrom",  # Europe/Rome
-        "nlams",  # Europe/Amsterdam
-        "plwaw",  # Europe/Warsaw
-        # Canada
-        "cator",  # America/Toronto
-        # Australia
-        "aubne",  # Australia/Brisbane
-        "ausyd",  # Australia/Sydney
-        # Brazil
-        "brsao",  # America/Sao_Paulo
-        # Japan
-        "jptyo",  # Asia/Tokyo
-        # China
-        "cnsha",  # Asia/Shanghai
-    )
-
     def __init__(self, bot: Mipha) -> None:
         self.bot: Mipha = bot
-        self.valid_timezones: set[str] = zoneinfo.available_timezones()
-        self._timezone_aliases: dict[str, str] = {
-            "Eastern Time": "America/New_York",
-            "Central Time": "America/Chicago",
-            "Mountain Time": "America/Denver",
-            "Pacific Time": "America/Los_Angeles",
-            # (Unfortunately) special case American timezone abbreviations
-            "EST": "America/New_York",
-            "CST": "America/Chicago",
-            "MST": "America/Denver",
-            "PST": "America/Los_Angeles",
-            "EDT": "America/New_York",
-            "CDT": "America/Chicago",
-            "MDT": "America/Denver",
-            "PDT": "America/Los_Angeles",
-        }
-        self._default_timezones: list[app_commands.Choice[str]] = []
-
-    async def cog_load(self) -> None:
-        await self.parse_bcp47_timezones()
 
     async def cog_command_error(self, ctx: Context, error: commands.CommandError) -> None:
         """Error handling for Time.py."""
         error = getattr(error, "original", error)
         if isinstance(error, commands.BadArgument):
             await ctx.send(str(error))
-
-    async def parse_bcp47_timezones(self) -> None:
-        async with self.bot.session.get(
-            "https://raw.githubusercontent.com/unicode-org/cldr/main/common/bcp47/timezone.xml",
-        ) as resp:
-            if resp.status != 200:
-                return
-
-            parser = etree.XMLParser(ns_clean=True, recover=True, encoding="utf-8")
-            tree = etree.fromstring(await resp.read(), parser=parser)  # noqa: S320 # trusted data source
-
-            # Build a temporary dictionary to resolve "preferred" mappings
-            entries: dict[str, CLDRDataEntry] = {
-                node.attrib["name"]: CLDRDataEntry(
-                    description=node.attrib["description"],
-                    aliases=node.get("alias", "Etc/Unknown").split(" "),
-                    deprecated=node.get("deprecated", "false") == "true",
-                    preferred=node.get("preferred"),
-                )
-                for node in tree.iter("type")
-                # Filter the Etc/ entries (except UTC)
-                if not node.attrib["name"].startswith(("utcw", "utce", "unk"))
-                and not node.attrib["description"].startswith("POSIX")
-            }
-
-            for entry in entries.values():
-                # These use the first entry in the alias list as the "canonical" name to use when mapping the
-                # timezone to the IANA database.
-                # The CLDR database is not particularly correct when it comes to these, but neither is the IANA database.
-                # It turns out the notion of a "canonical" name is a bit of a mess. This works fine for users where
-                # this is only used for display purposes, but it's not ideal.
-                if entry.preferred is not None:
-                    preferred = entries.get(entry.preferred)
-                    if preferred is not None:
-                        self._timezone_aliases[entry.description] = preferred.aliases[0]
-                else:
-                    self._timezone_aliases[entry.description] = entry.aliases[0]
-
-            for key in self.DEFAULT_POPULAR_TIMEZONE_IDS:
-                entry = entries.get(key)
-                if entry is not None:
-                    self._default_timezones.append(app_commands.Choice(name=entry.description, value=entry.aliases[0]))
 
     @cache()
     async def get_timezone(self, user_id: int, /) -> str | None:
@@ -229,15 +96,6 @@ class Time(commands.Cog):
             tz = datetime.UTC
 
         return tz
-
-    def find_timezones(self, query: str) -> list[TimeZone]:
-        # A bit hacky, but if '/' is in the query then it's looking for a raw identifier
-        # otherwise it's looking for a CLDR alias
-        if "/" in query:
-            return [TimeZone(key=a, label=a) for a in finder(query, self.valid_timezones)]
-
-        keys = finder(query, self._timezone_aliases.keys())
-        return [TimeZone(label=k, key=self._timezone_aliases[k]) for k in keys]
 
     def _gen_tz_embeds(self, requester: str, iterable: list[str]) -> list[discord.Embed]:
         embeds = []
@@ -355,9 +213,9 @@ class Time(commands.Cog):
     @_set.autocomplete("timezone")
     async def timezone_autocomplete_callback(self, interaction: Interaction, current: str) -> list[app_commands.Choice[str]]:
         if not current:
-            return self._default_timezones
+            return self.bot.tz_handler._default_timezones
 
-        matches = self.find_timezones(current)
+        matches = self.bot.tz_handler.find_timezones(current)
         return [tz.to_choice() for tz in matches[:25]]
 
     async def time_error(self, ctx: Context, error: commands.CommandError) -> None:
