8b30c1a124eeb34e530669c0a66781ffb4402347
Alex Nørgaard <umbra@abstractumbra.dev>
2025-06-02 00:48:58 +0100

    Revert "remove non-used extensions"
    This reverts commit bd4107af2ee1a15d47e58cd285861da875d191e7.

---
+++ extensions/_voice_stalking.py
@@ -0,0 +1,216 @@
+from __future__ import annotations
+
+import datetime
+import pathlib
+from enum import Enum
+from typing import TYPE_CHECKING, Any, TypedDict
+
+import discord
+from discord.ext import commands
+
+from utilities.shared.async_config import Config
+from utilities.shared.checks import has_guild_permissions
+
+if TYPE_CHECKING:
+    from bot import Mipha
+    from utilities.context import Context, GuildContext
+
+    VocalGuildChannel = discord.VoiceChannel | discord.StageChannel
+
+
+class BadStalkingConfig(commands.UserInputError):
+    def __init__(self, guild: discord.Guild, *args: Any) -> None:
+        message = f"Bad config in {guild} ({guild.id}) for voice stalking."
+        super().__init__(message, *args)
+
+
+class VoiceStateType(Enum):
+    connect = 0
+    disconnect = 1
+    move = 2
+
+
+class VoiceStalkingConfig(TypedDict):
+    notification_channel: int
+    excluded_channels: list[int]
+    filtered: bool
+
+
+COLOUR_LOOKUP: dict[VoiceStateType, discord.Colour] = {
+    VoiceStateType.connect: discord.Colour.green(),
+    VoiceStateType.disconnect: discord.Colour.red(),
+    VoiceStateType.move: discord.Colour.blurple(),
+}
+NORMALIZE_STATE: dict[VoiceStateType, str] = {
+    VoiceStateType.connect: "joined",
+    VoiceStateType.disconnect: "left",
+    VoiceStateType.move: "moved",
+}
+
+
+class VoiceStalking(commands.Cog):
+    def __init__(self, bot: Mipha) -> None:
+        self.bot: Mipha = bot
+        self._config: Config[VoiceStalkingConfig] = Config(pathlib.Path("configs/voice_stalking.json"))
+
+    async def cog_check(self, ctx: Context) -> bool:
+        return await ctx.bot.is_owner(ctx.author)
+
+    def _create_default_config(self) -> VoiceStalkingConfig:
+        ret: VoiceStalkingConfig = {"notification_channel": 0, "excluded_channels": [], "filtered": False}
+
+        return ret
+
+    def _is_safe_channel(self, channel: VocalGuildChannel) -> bool:
+        config: VoiceStalkingConfig = self._config.get(channel.guild.id, {})  # type: ignore # dumb
+        if not config:
+            return False
+
+        if config["filtered"] is False:
+            return True
+
+        excluded_channels: list[int] = config["excluded_channels"]
+        if channel.id in excluded_channels:
+            return False
+
+        default_role = channel.guild.default_role
+        if channel.permissions_for(default_role).view_channel is False:
+            return False
+
+        return True
+
+    def generate_embed(
+        self,
+        member: discord.Member,
+        /,
+        *,
+        state: VoiceStateType,
+        before: VocalGuildChannel | None,
+        after: VocalGuildChannel | None,
+    ) -> discord.Embed:
+        embed = discord.Embed(colour=COLOUR_LOOKUP[state])
+        embed.set_author(name=member.name, icon_url=member.display_avatar.url)
+
+        normalised = NORMALIZE_STATE[state]
+        if state is VoiceStateType.connect:
+            assert after is not None
+            fmt = f"{member.mention} has {normalised} {after.mention}."
+        elif state is VoiceStateType.disconnect:
+            assert before is not None
+            fmt = f"{member.mention} has {normalised} {before.mention}."
+        elif state is VoiceStateType.move:
+            assert before is not None
+            assert after is not None
+            fmt = f"{member.mention} has {normalised} from {before.mention} to {after.mention}."
+
+        embed.description = fmt
+        embed.timestamp = datetime.datetime.now(datetime.UTC)
+
+        return embed
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(
+        self,
+        member: discord.Member,
+        before: discord.VoiceState,
+        after: discord.VoiceState,
+    ) -> None:
+        config: VoiceStalkingConfig = self._config.get(member.guild.id, {})  # type: ignore # dumb
+        if not config:
+            return
+
+        if before.channel is None and after.channel is not None:
+            state = VoiceStateType.connect
+        elif before.channel and after.channel:
+            if before.channel == after.channel:
+                return
+            state = VoiceStateType.move
+        elif before.channel is not None and after.channel is None:
+            state = VoiceStateType.disconnect
+        else:
+            raise RuntimeError("Unreachable code in voice stalking.")
+
+        channel_id = config["notification_channel"]
+        channel: discord.TextChannel | None = self.bot.get_channel(channel_id)  # type: ignore # only text channels will be allowed.
+        if channel is None:
+            raise BadStalkingConfig(member.guild)
+
+        if state is VoiceStateType.move:
+            if self._is_safe_channel(before.channel) and self._is_safe_channel(after.channel):  # type: ignore # this is guarded by the state above
+                state = VoiceStateType.move
+            elif self._is_safe_channel(before.channel) and not self._is_safe_channel(after.channel):  # type: ignore # this is guarded by the state above
+                state = VoiceStateType.disconnect
+            elif not self._is_safe_channel(before.channel) and self._is_safe_channel(after.channel):  # type: ignore # this is guarded by the state above
+                state = VoiceStateType.connect
+        if state is VoiceStateType.connect and not self._is_safe_channel(after.channel):  # type: ignore # this is guarded by the state above
+            return
+        if state is VoiceStateType.disconnect and not self._is_safe_channel(before.channel):  # type: ignore # this is guarded by the state above
+            return
+
+        embed = self.generate_embed(member, state=state, before=before.channel, after=after.channel)
+        await channel.send(embed=embed)
+
+    @commands.group()
+    async def stalking(self, ctx: Context) -> None:
+        """Voice stalking parent command."""
+        if ctx.invoked_subcommand is None:
+            await ctx.send_help(ctx.command)
+
+    @stalking.command()
+    @commands.guild_only()
+    async def setup(self, ctx: GuildContext) -> None:
+        """Set up this guild for voice stalking."""
+        config = self._config.get(ctx.guild.id)
+        if config:
+            return await ctx.send("It seems it's already set up here?")
+
+        config = self._create_default_config()
+        await self._config.put(ctx.guild.id, config)
+
+        await ctx.message.add_reaction(ctx.tick(True))
+
+    @stalking.command()
+    @commands.guild_only()
+    @has_guild_permissions(manage_channels=True)
+    async def exclude(
+        self,
+        ctx: GuildContext,
+        channel: discord.VoiceChannel | discord.StageChannel | discord.Object,
+    ) -> None:
+        """Exclude a channel from the voice stalking."""
+        config = self._config.get(ctx.guild.id)
+        if not config:
+            return
+
+        config["excluded_channels"].append(channel.id)
+        await self._config.put(ctx.guild.id, config)
+
+    @stalking.command()
+    @commands.guild_only()
+    @has_guild_permissions(manage_channels=True)
+    async def create(self, ctx: GuildContext, name: str, exclude: commands.Greedy[discord.Member | discord.Role]) -> None:
+        """Create an excluded text and voice channel combination."""
+        config: VoiceStalkingConfig | None = self._config.get(ctx.guild.id)
+        if not config:
+            return
+
+        no_perms_overwrite = discord.PermissionOverwrite.from_pair(discord.Permissions.none(), discord.Permissions.all())
+        overwrites = {item: no_perms_overwrite for item in exclude}
+
+        tc = await ctx.guild.create_text_channel(name=name, overwrites=overwrites)
+        vc = await ctx.guild.create_voice_channel(name=name, overwrites=overwrites)
+
+        config["excluded_channels"].append(vc.id)
+        await self._config.put(ctx.guild.id, config)
+
+        try:
+            await ctx.author.send(
+                f"Okay, I have created {tc.mention} and {vc.mention} for you, and excluded the following:-\n\n"
+                + "\n".join([i.mention for i in exclude]),
+            )
+        except discord.HTTPException:
+            pass
+
+
+async def setup(bot: Mipha) -> None:
+    await bot.add_cog(VoiceStalking(bot))
---
+++ extensions/rng.py
@@ -0,0 +1,207 @@
+from __future__ import annotations
+
+import random as rng
+from collections import Counter
+from typing import TYPE_CHECKING, Annotated
+
+from discord.ext import commands
+
+from utilities.shared.formats import plural
+
+if TYPE_CHECKING:
+    from bot import Mipha
+    from utilities.context import Context
+
+
+class RNG(commands.Cog):
+    """Utilities that provide pseudo-RNG."""
+
+    def __init__(self, bot: Mipha, /) -> None:
+        self.bot: Mipha = bot
+
+    @commands.group()
+    async def random(self, ctx: Context) -> None:
+        """Displays a random thing you request."""
+        if ctx.invoked_subcommand is None:
+            await ctx.send(f"Incorrect random subcommand passed. Try {ctx.prefix}help random")
+
+    @random.command()
+    async def number(self, ctx: Context, minimum: int = 0, maximum: int = 100) -> None:
+        """Displays a random number within an optional range.
+
+        The minimum must be smaller than the maximum and the maximum number
+        accepted is 1000.
+        """
+
+        maximum = min(maximum, 1000)
+        if minimum >= maximum:
+            await ctx.send("Maximum is smaller than minimum.")
+            return
+
+        await ctx.send(str(rng.randint(minimum, maximum)))
+
+    @random.command()
+    async def lenny(self, ctx: Context) -> None:
+        """Displays a random lenny face."""
+        lenny = rng.choice(
+            [
+                "( ͡° ͜ʖ ͡°)",
+                "( ͠° ͟ʖ ͡°)",
+                "ᕦ( ͡° ͜ʖ ͡°)ᕤ",
+                "( ͡~ ͜ʖ ͡°)",
+                "( ͡o ͜ʖ ͡o)",
+                "͡(° ͜ʖ ͡ -)",
+                "( ͡͡ ° ͜ ʖ ͡ °)﻿",
+                "(ง ͠° ͟ل͜ ͡°)ง",
+                "ヽ༼ຈل͜ຈ༽ﾉ",
+            ],
+        )
+        await ctx.send(lenny)
+
+    @commands.command()
+    async def choose(self, ctx: Context, *choices: Annotated[str, commands.clean_content]) -> None:
+        """Chooses between multiple choices.
+
+        To denote multiple choices, you should use double quotes.
+        """
+        if len(choices) < 2:
+            return await ctx.send("Not enough choices to pick from.")
+
+        await ctx.send(rng.choice(choices))
+
+    def _bestof_choices(self, first: str, second: str, best_of: int) -> tuple[str, list[str]]:
+        """Plays a best of N game between two choices and returns the status of each game in a list."""
+        wins_needed = (best_of // 2) + 1
+        wins: list[int] = [0, 0]
+        results: list[str] = []
+        for _ in range(best_of):
+            winner = rng.choice([0, 1])
+            wins[winner] += 1
+            choice = first if winner == 0 else second
+            results.append(choice)
+            if wins[winner] == wins_needed:
+                return (choice, results)
+
+        raise RuntimeError("Unreachable")
+
+    def _simulate_double_elimination(self, first: str, second: str, third: str) -> list[str]:
+        """Simulates a double elimination tournament between three choices."""
+
+        # Bracket visualisation:
+        # T1 vs T2 => W1
+        # T3 vs W1 => W2
+        # L1 vs L2 => W3
+        # W2 vs W3 => W4
+        # if W2 wins => champion
+        # if W3 wins, W2 vs W3 again for champion
+
+        to_send: list[str] = []
+        # First round is Bo3
+        winner, results = self._bestof_choices(first, second, 3)
+        formatted_results = ", ".join("Win" if r == winner else "Loss" for r in results)
+        to_send.append(f"1. {first} vs {second}: {winner} wins! ({formatted_results})")
+        loser = first if winner == second else second
+        # Second round is also Bo3
+        second_winner, results = self._bestof_choices(winner, third, 3)
+        formatted_results = ", ".join("Win" if r == second_winner else "Loss" for r in results)
+        to_send.append(f"2. {winner} vs {third}: {second_winner} wins! ({formatted_results})")
+
+        # Third round is loser's bracket Bo3
+        second_loser = winner if second_winner == third else third
+        third_winner, results = self._bestof_choices(loser, second_loser, 3)
+        formatted_results = ", ".join("Win" if r == third_winner else "Loss" for r in results)
+        to_send.append(f"3. {loser} vs {second_loser}: {third_winner} wins! ({formatted_results})")
+        eliminated = loser if third_winner == second_loser else second_loser
+        to_send.append(f"  - {eliminated} is eliminated!")
+
+        # Championship rounds are Bo5
+        fourth_winner, results = self._bestof_choices(second_winner, third_winner, 5)
+        formatted_results = ", ".join("Win" if r == fourth_winner else "Loss" for r in results)
+        if fourth_winner == second_winner:
+            to_send.append(
+                f"4. {second_winner} vs {third_winner}: **{fourth_winner} won the championship ({formatted_results})!**",
+            )
+            return to_send
+        else:
+            to_send.append(f"4. {second_winner} vs {third_winner}: {fourth_winner} wins! ({formatted_results})")
+
+        # Upset round
+        champion, results = self._bestof_choices(second_winner, third_winner, 5)
+        formatted_results = ", ".join("Win" if r == champion else "Loss" for r in results)
+        to_send.append(f"5. **{champion} won the championship ({formatted_results})!**")
+        return to_send
+
+    def generate_round_robin(self, choices: list[str | None]) -> list[list[tuple[str | None, str | None]]]:
+        """Generates a round robin tournament between the choices."""
+        # Bye marker
+        if len(choices) % 2:
+            choices.append(None)
+
+        half = len(choices) // 2
+        schedule: list[list[tuple[str | None, str | None]]] = []
+        for _ in range(len(choices) - 1):
+            schedule.append([(choices[j], choices[-j - 1]) for j in range(half)])
+            choices.insert(1, choices.pop())
+
+        return schedule
+
+    def simulate_round_robin(self, choices: list[str]) -> list[str]:
+        """Simulates a round robin tournament between the choices."""
+        schedule = self.generate_round_robin(choices)  # type: ignore
+        to_send: list[str] = []
+        winners = Counter()
+        for index, round in enumerate(schedule, start=1):
+            to_send.append(f"**Round {index}:**")
+            for first, second in round:
+                if first is None:
+                    to_send.append(f"- {second} gets a bye!")
+                    winners[second] += 1
+                    continue
+
+                if second is None:
+                    to_send.append(f"- {first} gets a bye!")
+                    winners[first] += 1
+                    continue
+
+                winner, results = self._bestof_choices(first, second, 3)
+                formatted_results = ", ".join("Win" if r == winner else "Loss" for r in results)
+                to_send.append(f"- {first} vs {second}: {winner} wins! ({formatted_results})")
+                winners[winner] += 1
+
+        to_send.append("**Final Results:**")
+        for winner, wins in winners.most_common():
+            to_send.append(f"- {winner} has {plural(wins):win}")
+
+        return to_send
+
+    @commands.command()
+    async def choosebestof(
+        self,
+        ctx: Context,
+        *choices: Annotated[str, commands.clean_content(escape_markdown=True)],
+    ) -> None:
+        """Chooses between multiple choices in a tournament style."""
+        if len(choices) < 2:
+            return await ctx.send("Not enough choices to pick from.")
+
+        if len(choices) > 10:
+            return await ctx.send("Too many choices to pick from.")
+
+        results: list[str] = []
+        if len(choices) == 2:
+            first, second = choices
+            winner, games = self._bestof_choices(first, second, 5)
+            results.append(f"{first} vs {second}:")
+            for index, result in enumerate(games, start=1):
+                results.append(f"Round {index}: {result} wins")
+            results.append(f"**{winner} wins**")
+        elif len(choices) == 3:
+            results = self._simulate_double_elimination(choices[0], choices[1], choices[2])
+        else:
+            results = self.simulate_round_robin(list(choices))
+
+        await ctx.send("\n".join(results))
+
+
+async def setup(bot: Mipha) -> None:
+    await bot.add_cog(RNG(bot))
